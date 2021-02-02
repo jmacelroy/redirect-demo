5d854b2b100dce377b1ddfcab31236009aa96805
Jacob MacElroy <30531294+jmacelroy@users.noreply.github.com>
2021-02-01 19:27:37 -0600

    changing service name

--- cmd/client-api/main.go
+++ cmd/client-api/main.go
@@ -15,7 +15,7 @@ func main() {
 		WriteTimeout: 60 * time.Second,
 		IdleTimeout:  120 * time.Second,
 		Addr:         ":8080",
-		Handler:      client.DefaultMux("rpc-server:8081"),
+		Handler:      client.DefaultMux("rpc-service:8081"),
 	}
 
 	fmt.Println("listening on :8080...")
