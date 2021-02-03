2f1ae552995c637e00b1acd65af1647823dc0724
Jacob MacElroy <30531294+jmacelroy@users.noreply.github.com>
2021-02-03 16:53:07 -0600

    Updating for move to http data server.

--- client-api/api.go
+++ client-api/api.go
@@ -1,46 +1,35 @@
 package client
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-
-	pb "github.com/jmacelroy/redirect-demo/loot_pb"
-	"google.golang.org/grpc"
 )
 
-func DefaultMux(rpcEndpoint string) *http.ServeMux {
-	handlers := Handlers{rpcEndpoint: rpcEndpoint}
+func DefaultMux(dataEndpoint string) *http.ServeMux {
+	handlers := Handlers{dataEndpoint: dataEndpoint}
 	mux := http.NewServeMux()
 	mux.Handle("/loot/description", http.HandlerFunc(handlers.LootData))
 	return mux
 }
 
 type Handlers struct {
-	rpcEndpoint string
+	dataEndpoint string
 }
 
 func (h Handlers) LootData(w http.ResponseWriter, r *http.Request) {
-	names, ok := r.URL.Query()["name"]
-	log.Printf("querying for description of %s\n", names)
+	// names, ok := r.URL.Query()["name"]
+	// log.Printf("querying for description of %s\n", names)
 
-	if !ok || len(names[0]) < 1 {
-		http.Error(w, "name query parameter missing", http.StatusBadRequest)
-		return
-	}
+	// if !ok || len(names[0]) < 1 {
+	// 	http.Error(w, "name query parameter missing", http.StatusBadRequest)
+	// 	return
+	// }
 
-	log.Printf("RPC Endpoint: %s\n", h.rpcEndpoint)
-	conn, err := grpc.Dial(h.rpcEndpoint, grpc.WithInsecure())
+	resp, err := http.Get(fmt.Sprintf("http://%s/", h.dataEndpoint))
 	if err != nil {
-		http.Error(w, fmt.Sprintf("rpc: %+v", err), http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("data: %+v", err), http.StatusInternalServerError)
 		return
-	}
-	client := pb.NewLootRPCClient(conn)
-	resp, err := client.LootDescription(context.Background(), &pb.LootDescriptionRequest{Name: names[0]})
-	if err != nil {
-		http.Error(w, fmt.Sprintf("rpc: %+v", err), http.StatusInternalServerError)
 	}
 
 	bytes, err := json.MarshalIndent(resp, "", "\t")
