2e422897aaf328786502a0f64c34aaad75dcf671
Jacob MacElroy <30531294+jmacelroy@users.noreply.github.com>
2021-02-04 13:27:18 -0600

    Envoy working in simple proxy.

--- cmd/loot-data/main.go
+++ cmd/loot-data/main.go
@@ -4,18 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	lootdata "github.com/jmacelroy/redirect-demo/loot-data"
 )
 
 func main() {
+	fullDataEnv := os.Getenv("DEMO_ENABLE_FULL_DATA")
+	var enableFullData bool
+	if fullDataEnv != "" {
+		enableFullData = true
+	}
+
 	server := &http.Server{
 		ReadTimeout:  60 * time.Second,
 		WriteTimeout: 60 * time.Second,
 		IdleTimeout:  120 * time.Second,
 		Addr:         ":8081",
-		Handler:      lootdata.DefaultMux(false),
+		Handler:      lootdata.DefaultMux(enableFullData),
 	}
 
 	fmt.Println("loot data server listening on :8081")
