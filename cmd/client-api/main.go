34c3c96f5e4eb30caa0b50d556c7025ce2120123
Jacob MacElroy <30531294+jmacelroy@users.noreply.github.com>
2021-02-10 16:26:56 -0600

    Adding env var for loot data endpoint.

--- cmd/client-api/main.go
+++ cmd/client-api/main.go
@@ -4,18 +4,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	client "github.com/jmacelroy/redirect-demo/client-api"
 )
 
 func main() {
+	lootDataEndpoint, ok := os.LookupEnv("LOOT_DATA_ENDPOINT")
+	if !ok {
+		log.Fatal("LOOT_DATA_ENDPOINT env var required")
+	}
 	server := &http.Server{
 		ReadTimeout:  60 * time.Second,
 		WriteTimeout: 60 * time.Second,
 		IdleTimeout:  120 * time.Second,
 		Addr:         ":8080",
-		Handler:      client.DefaultMux("loot-data.jacob:8081"),
+		Handler:      client.DefaultMux(lootDataEndpoint),
 	}
 
 	fmt.Println("listening on :8080...")
